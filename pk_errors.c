8cc8616a4af0a769e0abef8ddea9554bd9925065
kabandapatrick2580 <sendsome2019@gmail.com>
2023-08-24 20:11:21 +0200

    Monty Code

--- pk_errors.c
+++ pk_errors.c
@@ -1,42 +1,42 @@
 #include "monty.h"
 
 /**
- * open_error - A function that prints a file open error message.
- * @filename: Name of the file that failed to open.
- * Return: EXIT_FAILURE.
- */
+* open_error - A function that prints a file open error message.
+* @filename: Name of the file that failed to open.
+* Return: EXIT_FAILURE.
+*/
 int open_error(char *filename)
 {
-    fprintf(stderr, "Error: Can't open file %s\n", filename);
-    return (EXIT_FAILURE);
+fprintf(stderr, "Error: Can't open file %s\n", filename);
+return (EXIT_FAILURE);
 }
 
 /**
- * error_usage - A function that prints a usage error message.
- * @drapeau: Type of error: 0 malloc'd failed, or 1 usage.
- * Return: EXIT_FAILURE.
- */
+* error_usage - A function that prints a usage error message.
+* @drapeau: Type of error: 0 malloc'd failed, or 1 usage.
+* Return: EXIT_FAILURE.
+*/
 int error_usage(int drapeau)
 {
-    char *fr_errors[2] = {"Error: malloc failed", "USAGE: monty file"};
+char *fr_errors[2] = {"Error: malloc failed", "USAGE: monty file"};
 
-    fprintf(stderr, "%s\n", fr_errors[drapeau]);
-    return (EXIT_FAILURE);
+fprintf(stderr, "%s\n", fr_errors[drapeau]);
+return (EXIT_FAILURE);
 }
 
 /**
- * show_f_errors - A function that prints various errors.
- * @drapeau: The index to type of error.
- * @line_of_num: The number of line in file with error.
- * Return: EXIT_FAILURE.
- */
+* show_f_errors - A function that prints various errors.
+* @drapeau: The index to type of error.
+* @line_of_num: The number of line in file with error.
+* Return: EXIT_FAILURE.
+*/
 int show_f_errors(int drapeau, unsigned int line_of_num)
 {
-    char *fr_errors[] = {"usage: push integer", "can't pint, stack empty",
-                         "can't pop an empty stack", "can't swap, stack too short",
-                         /* ... (other error messages) */
-                         "can't pchar, stack empty"};
+char *fr_errors[] = {"usage: push integer", "can't pint, stack empty",
+"can't pop an empty stack", "can't swap, stack too short",
+/* ... (other error messages) */
+"can't pchar, stack empty"};
 
-    fprintf(stderr, "L%d: %s\n", line_of_num, fr_errors[drapeau]);
-    return (EXIT_FAILURE);
+fprintf(stderr, "L%d: %s\n", line_of_num, fr_errors[drapeau]);
+return (EXIT_FAILURE);
 }
--- pk_run.c
+++ pk_run.c
@@ -1,109 +1,109 @@
 #include "monty.h"
 
 /**
- * pk_run - A function that runs a Monty bytecodes script.
- * @dp: File descriptor for an open Monty bytecodes script.
- * Return: EXIT_SUCCESS on success, or EXIT_FAILURE on error.
- */
+* pk_run - A function that runs a Monty bytecodes script.
+* @dp: File descriptor for an open Monty bytecodes script.
+* Return: EXIT_SUCCESS on success, or EXIT_FAILURE on error.
+*/
 int pk_run(FILE *dp)
 {
-    stack_t *stack = NULL;
-    char *line = NULL, **token = NULL, delim[] = " \n\t\a\b";
-    size_t len = 0, mode = 1;
-    unsigned int linenum = 0, exit_status = EXIT_SUCCESS;
+stack_t *stack = NULL;
+char *line = NULL, **token = NULL, delim[] = " \n\t\a\b";
+size_t len = 0, mode = 1;
+unsigned int linenum = 0, exit_status = EXIT_SUCCESS;
 
-    while (fgets(line, len, dp) != NULL)
-    {
-        linenum++;
-        if (get_line(line, delim))
-            continue;
+while (fgets(line, len, dp) != NULL)
+{
+linenum++;
+if (get_line(line, delim))
+continue;
 
-        token = tok_en(line, delim);
-        if (token[0][0] == '#' || strcmp(token[0], "nop") == 0)
-        {
-            free(token);
-            continue;
-        }
-        else if (strcmp(token[0], "stack") == 0)
-            mode = 1;
-        else if (strcmp(token[0], "queue") == 0)
-            mode = 0;
-        else if (strcmp(token[0], "push") == 0 && mode == 0)
-            exit_status = pk_pushq(&stack, token, linenum);
-        else if (strcmp(token[0], "push") == 0 && mode == 1)
-            exit_status = pk_pushq(&stack, token, linenum);
-        else
-            exit_status = execute(token, &stack, linenum);
-        free(token);
-        if (exit_status == EXIT_FAILURE)
-            break;
-    }
-    stck_free(&stack);
-    free(line);
-    return (exit_status);
+token = tok_en(line, delim);
+if (token[0][0] == '#' || strcmp(token[0], "nop") == 0)
+{
+free(token);
+continue;
+}
+else if (strcmp(token[0], "stack") == 0)
+mode = 1;
+else if (strcmp(token[0], "queue") == 0)
+mode = 0;
+else if (strcmp(token[0], "push") == 0 && mode == 0)
+exit_status = pk_pushq(&stack, token, linenum);
+else if (strcmp(token[0], "push") == 0 && mode == 1)
+exit_status = pk_pushq(&stack, token, linenum);
+else
+exit_status = execute(token, &stack, linenum);
+free(token);
+if (exit_status == EXIT_FAILURE)
+break;
+}
+stck_free(&stack);
+free(line);
+return (exit_status);
 }
 
 /**
- * tok_en - A function that splits the line.
- * @line: The pointer to the bytecode line.
- * @delim: Array with 5 delimiters " \n\t\a\b".
- * Return: The pointer to tokens.
- */
+* tok_en - A function that splits the line.
+* @line: The pointer to the bytecode line.
+* @delim: Array with 5 delimiters " \n\t\a\b".
+* Return: The pointer to tokens.
+*/
 char **tok_en(char *line, char *delim)
 {
-    char *tokens = NULL, **token = NULL;
-    size_t bufsize = 0;
-    int i = 0;
+char *tokens = NULL, **token = NULL;
+size_t bufsize = 0;
+int i = 0;
 
-    if (line == NULL || !*line)
-        return (NULL);
+if (line == NULL || !*line)
+return (NULL);
 
-    bufsize = strlen(line);
-    if (bufsize == 0)
-        return (NULL);
-    token = malloc(bufsize * sizeof(char *));
-    if (token == NULL)
-    {
-        fprintf(stderr, "Memory allocation error\n");
-        exit(EXIT_FAILURE);
-    }
-    tokens = strtok(line, delim);
-    if (tokens == NULL)
-    {
-        free(token);
-        return (NULL);
-    }
-    while (tokens != NULL)
-    {
-        token[i] = tokens;
-        i++;
-        tokens = strtok(NULL, delim);
-    }
-    token[i] = NULL;
-    return (token);
+bufsize = strlen(line);
+if (bufsize == 0)
+return (NULL);
+token = malloc(bufsize *sizeof(char *));
+if (token == NULL)
+{
+fprintf(stderr, "Memory allocation error\n");
+exit(EXIT_FAILURE);
+}
+tokens = strtok(line, delim);
+if (tokens == NULL)
+{
+free(token);
+return (NULL);
+}
+while (tokens != NULL)
+{
+token[i] = tokens;
+i++;
+tokens = strtok(NULL, delim);
+}
+token[i] = NULL;
+return (token);
 }
 
 /**
- * get_line - A function that checks if the line only contains delimiters.
- * @line: The pointer to the line.
- * @delims: The string with delimiter characters.
- * Return: 1 if the line only contains delimiters, otherwise 0.
- */
+* get_line - A function that checks if the line only contains delimiters.
+* @line: The pointer to the line.
+* @delims: The string with delimiter characters.
+* Return: 1 if the line only contains delimiters, otherwise 0.
+*/
 int get_line(char *line, char *delims)
 {
-    int i, j;
+int i, j;
 
-    for (i = 0; line[i]; i++)
-    {
-        for (j = 0; delims[j]; j++)
-        {
-            if (line[i] == delims[j])
-                break;
-        }
-        if (delims[j] == '\0')
-            return (0);
-    }
+for (i = 0; line[i]; i++)
+{
+for (j = 0; delims[j]; j++)
+{
+if (line[i] == delims[j])
+break;
+}
+if (delims[j] == '\0')
+return (0);
+}
 
-    return (1);
+return (1);
 }
 
